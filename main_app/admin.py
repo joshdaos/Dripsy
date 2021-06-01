62cb9b8f22eee85c807177797d57875c768f676f
JoshDaos <joshuadaos1998@gmail.com>
2021-06-01 16:46:56 -0700

    created models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import SellerProfile, SellersFollowed, Products
 # Register your models here.
+
+admin.site.register(SellerProfile)
+admin.site.register(SellersFollowed)
+admin.site.register(Products)
