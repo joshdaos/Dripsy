64b611f286a56ba73d1d54998f99e90d8e6aa281
JoshDaos <joshuadaos1998@gmail.com>
2021-06-01 12:52:03 -0700

    working on nav bar and anchors

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View  
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+
+class Home(TemplateView):
+
+    template_name = "home.html"
+
+
+class Feed(TemplateView):
+    template_name = "feed.html"
+
+
+class UserProfile(TemplateView):
+    template_name = "profile_detail.html"
