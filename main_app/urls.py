df6559e68936debba1ddf47675f2dfbc500a7f8a
JoshDaos <joshuadaos1998@gmail.com>
2021-06-02 15:23:22 -0700

    fixed db relations & added profile_detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('feed/', views.Feed.as_view(), name="feed"),
-    path('profile/', views.UserProfile.as_view(), name="profile"),
+    path('feed/', views.SellerList.as_view(), name="feed"),
+    path('profile/', views.UserProfile.as_view(), name="profile_detail"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View  
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
-
+from .models import SellerProfile
 # Create your views here.
 
 
@@ -11,9 +11,16 @@ class Home(TemplateView):
     template_name = "home.html"
 
 
-class Feed(TemplateView):
-    template_name = "feed.html"
-
-
 class UserProfile(TemplateView):
     template_name = "profile_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["profile"] = SellerProfile.objects.get(user=self.request.user)
+        return context
+
+
+class SellerList(TemplateView):
+    template_name = "seller_list.html"
+
+
