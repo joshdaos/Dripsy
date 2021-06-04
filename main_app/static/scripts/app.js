f2fa2faad360edbed0372e34a87bc2b584895535
JoshDaos <joshuadaos1998@gmail.com>
2021-06-04 15:30:39 -0700

    css in landing page

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -1,11 +1,4 @@
-const zoomElement = document.querySelector(".workspace");
-let zoom = 1;
-const ZOOM_SPEED = 0.1;
+console.log("javascript");
 
-document.addEventListener("wheel", function (e) {
-  if (e.deltaY > 0) {
-    zoomElement.style.transform = `scale(${(zoom += ZOOM_SPEED)})`;
-  } else {
-    zoomElement.style.transform = `scale(${(zoom -= ZOOM_SPEED)})`;
-  }
-});
+
+
