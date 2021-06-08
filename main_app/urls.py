64cf4a7d3e950fa47c3545e0ba1d8a283236828a
JoshDaos <joshuadaos1998@gmail.com>
2021-06-08 12:01:15 -0700

    css fun

--- main_app/urls.py
+++ main_app/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('product/<int:pk>/delete', views.ProductDelete.as_view(), name="product_delete"),
     path('product/<int:pk>/update', views.ProductUpdate.as_view(), name="product_update"),
     path('accounts/signup/', views.Signup.as_view(), name="signup"),
+    path('seller/<int:pk>/', views.SellerDetail.as_view(), name="seller_detail"),
 ]
